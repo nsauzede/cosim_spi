76a3e1f05f7dde630602c42f25c95592d102edd3
Nicolas Sauzede <nicolas.sauzede@gmail.com>
2025-04-09 10:25:35 +0200

    Enhance spimasterreset-glitch to set divcoef/spi3w

--- ns_counter/tb1/spi_master.v
+++ ns_counter/tb1/spi_master.v
@@ -21,25 +21,42 @@
 /*
     SPI master
     - 3/4-wire mode support (4-wire by default)
-    - flexible divider coefficient (see below)
-
-    Define `SPI3WIRE to enable 3-wire mode support,
-    and set .spi3w=1 to switch the SPI master to 3-wire mode.
-    (SPI slave must also be set to 3-wire by some means accordingly of course)
+    - flexible divider coefficient
+    Note that it is mainly targeting the STMicro LIS3DH sensor SPI protocol.
+    In particular:
+    - SPI clock polarity/phase
+    - 3-wire mode (ie: tristate window)
+    Maybe there are some specifics that should/could be reworked to make it more general..
 
     Divider coefficient (10000 by default when DIV_COEF==0) can be changed while in reset:
     - maintain nrst=0
-     - set nbits=0
-     - set mosi_data[15:0]=divider (eg: 16'd20000)
+     - set nbits=5'd31
+     - set mosi_data[31:16]=16'h8080    `set divcoef as mosi_data[15:0]`
+     - set mosi_data[15:0]=divider      (eg: 16'h4e20 (20000))
      - set request=1
     - after some clocks:
      - set request=0
+     - release nrst and set request/nbits/mosi_data as needed
+
+    Define `SPI3WIRE to configure 3-wire mode support and enable it (see below) to switch the SPI master to 3-wire mode.
+    (SPI slave must also be set to 3-wire by some means accordingly of course)
+    To enable the 3-wire mode (or disable it to standard/default 4-wire mode):
+    - maintain nrst=0
+     - set nbits=5'd31
+     - Either:
+        - set mosi_data[31:0]=16'h81010000 `set 3-wire on`
+     - Or:
+        - set mosi_data[31:0]=16'h81000000 `set 3-wire off (default 4-wire)`
+     - set request=1
+    - after some clocks:
+     - set request=0
+     - release nrst and set request/nbits/mosi_data as needed
 */
 module spi_master #( parameter integer DIV_COEF = 0 ) (
     input               clk_in,         // Logic clock
     input               nrst,           // SPI is active when nreset is HIGH
 
-    input               request,        // Request to start transfer: Active HIG
+    input               request,        // Request to start transfer: Active HIGH
     input   [4:0]       nbits,          // Number of bits: nbits==0 means 1 bit
     input  [31:0]       mosi_data,      // Parallel FPGA data write to SPI
     output [31:0]       miso_data,      // Parallel FPGA data read from SPI
@@ -48,9 +65,7 @@ module spi_master #( parameter integer DIV_COEF = 0 ) (
     output              spi_csn,        // SPI CSN output (active LOW)
     output              spi_sck,        // SPI clock output
 `ifdef SPI3WIRE
-    input               spi3w,          // 0=4-wire (default/standard), 1=3-wire
-    inout               spi_mosi,       // spi3w=0: SPI master data output, slave data input
-                                        // spi3w=1: SPI master/slave data output/input
+    inout               spi_mosi,       // SPI master output slave input (default 4-wire); SPI master/slave output/input (3-wire enabled)
 `else
     output              spi_mosi,       // SPI master data output, slave data input
 `endif
@@ -79,14 +94,16 @@ module spi_master #( parameter integer DIV_COEF = 0 ) (
     reg [4:0] nbits_reg = 0;
     reg [4:0] bit_counter = 0;
 `ifdef SPI3WIRE
-    //reg spi3w = 1'b0;
+    reg spi3w = 1'b0;
     reg oe = 1'b1;
+    reg rd = 1'b0;
 //    assign spi_mosi = spi3w ? (oe ? mosiff : 1'bz) : mosiff;
 //    assign spi_mosi = spi3w && !oe ? 1'bz : mosiff;
     assign spi_mosi = !oe ? 1'bz : mosiff;
 `else
-    reg spi3w = 1;              // should remove
+    reg spi3w = 0;              // should remove
     reg oe = 1'b1;              // should remove
+    reg rd = 1'b0;              // should remove
     assign spi_mosi = mosiff;
 `endif
     assign ready = readyff;
@@ -102,7 +119,7 @@ module spi_master #( parameter integer DIV_COEF = 0 ) (
         if (nrst == 1'b0) begin
             divider <= 0;
             divider_out <= 0;
-            div_coef <= request && (nbits == 5'b0) ? mosi_data[15:0] : div_coef;
+            div_coef <= request && (nbits == 5'd31) && (mosi_data[31] && mosi_data[23]) ? mosi_data[15:0] : div_coef;
         end else begin
             if (divider <= div_coef) begin
                 divider <= divider + 16'd1;
@@ -117,7 +134,7 @@ module spi_master #( parameter integer DIV_COEF = 0 ) (
     always @(posedge clk_in or negedge nrst) begin
         if (nrst == 1'b0) begin
 `ifdef SPI3WIRE
-            oe <= 1;
+            spi3w <= request && (nbits == 5'd31) && (mosi_data[31] && mosi_data[24]) ? mosi_data[16] : spi3w;
 `endif
             csnff <= 1;
             sckff <= 1;
@@ -133,6 +150,7 @@ module spi_master #( parameter integer DIV_COEF = 0 ) (
                 STATE_Idle: begin
 `ifdef SPI3WIRE
                     oe <= 1;
+                    rd <= 0;
 `endif
                     if (request) begin
                         mosi_reg <= mosi_data;
@@ -145,6 +163,9 @@ module spi_master #( parameter integer DIV_COEF = 0 ) (
                 end
                 STATE_Run: begin
                     if (nbits_reg == 5'd31) begin
+`ifdef SPI3WIRE
+                        rd <= mosi_reg[31];
+`endif
                         state <= STATE_High;
                     end else begin
                         mosi_reg <= mosi_reg << 1;
@@ -157,7 +178,7 @@ module spi_master #( parameter integer DIV_COEF = 0 ) (
                     state <= STATE_Low;
 `ifdef SPI3WIRE
                         if (bit_counter == (nbits - 5'd8)) begin
-                            if (spi3w) begin
+                            if (spi3w && rd) begin
                                 oe <= 0;
                             end
                         end
@@ -175,7 +196,9 @@ module spi_master #( parameter integer DIV_COEF = 0 ) (
                     end else begin
 `ifdef SPI3WIRE_
                         if (bit_counter == (nbits - 5'd8)) begin
-                            oe <= 0;
+                            if (spi3w && rd) begin
+                                oe <= 0;
+                            end
                         end
 `endif
                         bit_counter <= bit_counter - 5'd1;
