80db4fd600f1136308da61ca05b3888810b883a9
Nicolas Sauzede <nicolas.sauzede@gmail.com>
2025-04-16 22:02:35 +0200

    Remove ns_counter (transferred back to papilio_one_500k)

--- ns_counter/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ut test && for p in `ls`; do
-    ([ -d $p ] && cd $p && rm -f *.vcd)
-done
--- ns_counter/go.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./setup.sh
-ut test && for p in `ls`; do
-    ([ -d $p ] && cd $p && ../test.sh && ../run.sh)
-done
